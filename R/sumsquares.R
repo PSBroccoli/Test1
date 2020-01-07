838d36d6240a49692e0b91ef48a8d5003b7661d7
PSBroccoli <lw944@york.ac.uk>
2020-01-07 12:22:03 +0000

    Added documentation

--- R/sumsquares.R
+++ R/sumsquares.R
@@ -1,3 +1,16 @@
+#' Sum of Squares
+#'
+#' Calculates the sum of squared deviations from mean
+#'
+#' @param x numeric vector
+#'
+#' @return number
+#' @export
+#'
+#' @examples
+#' sumsquares(rnorm(50))
 sumsquares <- function(x){
   sum((mean(x))^2)
 }
+
+
